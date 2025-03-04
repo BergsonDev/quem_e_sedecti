ce26558c1e0481a50a4b3b54631ebf737878ae48
BergsonDev <trabalhocodigo@outlook.com>
2025-03-04 15:48:12 -0400

    Adição do js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+window.addEventListener("load",()=>{
+    document.querySelector("header").style.display = "";
+    document.querySelector(".content-page").style.display = "";
+    document.querySelector("footer").style.display = "";
+    document.querySelector(".load").style.display = "none";
+})
